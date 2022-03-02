f7ea4e30a0691c3b53945fc26faea67c595272ac
Adriana Čejka <adriana.cejka@seznam.cz>
2022-03-02 15:54:23 +0100

    promenna aktualni pozice panacka

--- main.js
+++ main.js
@@ -7,3 +7,37 @@ if (!( panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || pan
 
 
 // sem začni psát svůj program
+
+function stiskKlavesy(udalost) {
+	console.log(udalost.key);
+
+}
+
+
+let panacek = document.querySelector('#panacek');
+
+// panacek.style.left = '400px';
+// panacek.style.top = '200px';
+
+let pozicePanackaLeft = panacek.style.left = '50px';
+let pozicePanackaTop = panacek.style.top = '100px';
+
+let aktualniPanacekLeft = panacek.offsetLeft + 'px';
+console.log(aktualniPanacekLeft);
+
+let aktualniPanacekTop = panacek.offsetTop + 'px';
+console.log(aktualniPanacekTop);
+
+
+let krok
+
+
+udalost.key.ArrowDown
+udalost.key.ArrowRight
+udalost.key.ArrowUp
+udalost.key.ArrowLeft
+
+
+
+
+
