27c275d47c63091b8467950d049d688cc8f28b7e
Adriana Čejka <adriana.cejka@seznam.cz>
2022-03-10 20:45:43 +0100

    nefunkcni

--- main.js
+++ main.js
@@ -8,60 +8,73 @@ if (!( panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || pan
 
 // sem začni psát svůj program
 
-// ---------------- poloha panacka při startu
+// -------- poloha panacka při startu
 let panacek = document.querySelector('#panacek');
-panacek.style.left = '300px';
-panacek.style.top = '300px';
+panacek.style.left = '100px';
+panacek.style.top = '100px';
 
 document.querySelector('#hudba').volume = 0.3;
 
 // alert('Pro ultimátní zážitek ze hry povol přehrávání audia ve svém prohlížeči :)');
 
-  
-/* ---------------- aktualni poloha panacka
 
-	let aktualniPanacekLeft = panacek.offsetLeft + 'px';
-	console.log(aktualniPanacekLeft);
-
-	let aktualniPanacekTop = panacek.offsetTop + 'px';
-	console.log(aktualniPanacekTop);
+/* ---- funkce stisk klaves a hodnota
+function stiskKlavesy(udalost) {  
+console.log(udalost.key);	
+}
 */
 
-/* ---------------- funkce stisk klaves a hodnota
-
-	function stiskKlavesy(udalost) {  
-	console.log(udalost.key);	
-	}
-*/
-
-
+// ---- mince
 let mince = document.querySelector('#mince');
 let sirkaMince = 40;
 let vyskaMince = 40;
 
-let poziceMinceLeft = Math.floor(Math.random() * (window.innerWidth - sirkaMince)) + 'px';
-mince.style.left = poziceMinceLeft;
-console.log(poziceMinceLeft);
-
-let poziceMinceTop = Math.floor(Math.random() * (window.innerHeight - vyskaMince)) + 'px';
-mince.style.top = poziceMinceTop;
-console.log(poziceMinceTop);
-
-
+// ---- panacek
+let aktualniPozicePanacekLeft = panacek.offsetLeft;
+// console.log(aktualniPozicePanacekLeft);
+let aktualniPozicePanacekTop = panacek.offsetTop;
+// console.log(aktualniPozicePanacekTop);
 let panacekSirka = 64;
 let panacekVyska = 70;
 
-function stiskKlavesy(udalost) {
+let krok = 20;
+let poziceMinceLeft;
+let poziceMinceTop;
 
-	let aktualniPozicePanacekLeft = panacek.offsetLeft;
-	// console.log(aktualniPozicePanacekLeft);
-
-	let aktualniPozicePanacekTop = panacek.offsetTop;
-	// console.log(aktualniPozicePanacekTop);
-
-	let krok = 20;
+function umistiMinci() {
 	
+	mince.style.left = Math.floor(Math.random() * (window.innerWidth - sirkaMince)) + 'px';
+	console.log(poziceMinceLeft);
 
+	mince.style.top = Math.floor(Math.random() * (window.innerHeight - vyskaMince)) + 'px';
+	console.log(poziceMinceTop);
+}	
+
+function sezerMinci() {
+
+		// ---- zleva
+	if (!(parseInt(panacek.style.left) + panacekSirka < parseInt(poziceMinceLeft) || 
+		// ---- zprava
+	parseInt(poziceMinceLeft) + sirkaMince < parseInt(panacek.style.left) ||
+		// ---- shora
+	parseInt(panacek.style.top) + panacekVyska < parseInt(poziceMinceTop) ||
+		// ---- zespoda
+	parseInt(poziceMinceTop) + vyskaMince < parseInt(panacek.style.top))) {
+		
+		//panacek a mince se potkavaji
+		console.log('potkali se!');
+		// mince.style.display = 'none';
+		
+	}
+}
+
+
+function porovnejPozici()
+
+
+
+function stiskKlavesy(udalost) {
+	
 	if (udalost.keyCode === 40) {
 		// console.log('zmáčkla jsem šipku dolu');
 		panacek.style.top = aktualniPozicePanacekTop + krok + 'px';
@@ -107,22 +120,7 @@ function stiskKlavesy(udalost) {
 		}
 	}
 
-
-	// ----------- prolnuti pozic panacka a mince
-
-	if (!(parseInt(panacek.style.left) + panacekSirka < parseInt(poziceMinceLeft) || 
-	// ---- zprava
-	parseInt(poziceMinceLeft) + sirkaMince < parseInt(panacek.style.left) ||
-	// ---- shora
-	parseInt(panacek.style.top) + panacekVyska < parseInt(poziceMinceTop) ||
-	// ---- zespoda
-	parseInt(poziceMinceTop) + vyskaMince < parseInt(panacek.style.top))) {
-		
-		//panacek a mince se potkavaji
-		console.log('potkali se!');
-		mince.style.display = 'none';
-		
-	}
+	sezerMinci()
 
 }
 
